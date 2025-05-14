f1a30d14b7d7d3417ce9b2e476f606c1cb68cdae
Day-vilela <dayamano64@gmail.com>
2025-05-14 19:52:00 -0300

    Initial commit

---
+++ src/main/java/me/dio/minha/API/MinhaApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.minha.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/minha/API/MinhaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.minha.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
